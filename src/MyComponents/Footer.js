be3fa16757a34710cdc0bba7127567f5c0c3a62e
pratyush-kr <pratyush151198@gmail.com>
2021-12-14 23:55:59 +0530

    Footer 1:25:03

--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
 export const Footer = () => {
+    let footerStyle = {
+        position: "absolute",
+        top: '100vh',
+        width: '100%'
+    }
     return (
         <div>
-            Footer Works!
+            <footer className='bg-dark text-light py-3' style={footerStyle}>
+                <p className='text-center'>
+                    Copyrigh &copy; My 1st website
+                </p>
+            </footer> 
         </div>
     )
 }
--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -6,9 +6,10 @@ export const Todos = (props) => {
         <div className="container">
             <h3 className='text-center my-3'>Todos List</h3>
             {
-                props.todos.map((todo)=>{
-                    return <TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete}/>
-                })
+                props.todos.length === 0? "No Todos to display":
+                    props.todos.map((todo)=>{
+                        return <TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete}/>
+                    })
             }
         </div>
     )
