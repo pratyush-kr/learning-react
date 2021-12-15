ea5bcf0e573e640ed67050ba1cab7fdefed10b49
pratyush-kr <pratyush151198@gmail.com>
2021-12-15 11:48:38 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -3,34 +3,43 @@ import Header from "./MyComponents/Header"
 import {Todos} from "./MyComponents/Todos"
 import {Footer} from './MyComponents/Footer'
 import React, {useState} from 'react'
+import {AddTodo} from './MyComponents/AddTodo'
 
 function App() {
+  let initTodo
+  if(localStorage.getItem("todos") === null){
+    initTodo = []
+  }
+  else{
+    initTodo = JSON.parse(localStorage.getItem("todos"))
+  }
   const onDelete = (todo) =>{
     console.log("I am Deleted")
     setTodos(todos.filter((e)=>{
       return e!==todo
-    }))    
+    }))
+    localStorage.setItem("todos", JSON.stringify(todos))
   }
-  let [todos, setTodos] = useState([
-    {
-      sno: 1,
-      title: 'Go to market',
-      desc: 'Go to market to get this job done'
-    },
-    {
-      sno: 2,
-      title: 'go to the mall',
-      desc: 'Go to market to get this job done'
-    },
-    {
-      sno: 3,
-      title: 'Go to Ghat',
-      desc: 'Go to market to get this job done'
-    },
-  ])
+  const addTodo = (title, desc) =>{
+    console.log("I am adding this todo")
+    let sno
+    if(todos.length == 0)
+      sno = 0
+    else
+      sno = todos[todos.length-1].sno+1
+    const myTodo = {
+      sno: sno,
+      title: title,
+      desc: desc
+    }
+    setTodos([...todos, myTodo])
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }
+  let [todos, setTodos] = useState([initTodo])
   return (
     <div>
       <Header title='Todos List' searchBar={true}/>
+      <AddTodo addTodo={addTodo}/>
       <Todos todos={todos} onDelete={onDelete}/>
       <Footer/>
     </div>
--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -1,14 +1,9 @@
 import React from 'react'
 
 export const Footer = () => {
-    let footerStyle = {
-        position: "absolute",
-        top: '100vh',
-        width: '100%'
-    }
     return (
         <div>
-            <footer className='bg-dark text-light py-3' style={footerStyle}>
+            <footer className='bg-dark text-light py-3'>
                 <p className='text-center'>
                     Copyrigh &copy; My 1st website
                 </p>
